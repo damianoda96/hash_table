4358f210c33cfbb9372dcf0774d39f3ca623c5a3
Deven Damiano <damiano.da96@gmail.com>
2017-12-01 22:54:05 -0500

    hash table implemented

--- 316p4.cpp
+++ 316p4.cpp
@@ -1,9 +1,89 @@
 #include <iostream>
 
-struct hash_table
+struct Hash_Entry
 {
+    int key;
+    int value;
     
+    Hash_Entry(int key, int value)
+    {
+        this->key = key;
+        this->value = value;
+    }
+    
+    int getKey()
+    {
+        return key;
+    }
+    
+    int getValue()
+    {
+        return value;
+    }
+};
 
+const int TABLE_SIZE = 128;
+
+struct Hash_Map
+{
+    Hash_Entry ** table;
+    
+    Hash_Map()
+    {
+        table = new Hash_Entry* [TABLE_SIZE];
+        
+        for(int i = 0; i < TABLE_SIZE; i++)
+        {
+            table[i] = NULL;
+        }
+    }
+    
+    int get(int key)
+    {
+        int hash = (key % TABLE_SIZE);
+        
+        while(table[hash] != NULL && table[hash]->getKey() != key)
+        {
+            hash = (hash + 1) % TABLE_SIZE;
+        }
+        if(table[hash] == NULL)
+        {
+            return -1;
+        }
+        else
+        {
+            return table[hash]->getValue();
+        }
+    }
+    
+    void put(int key, int value)
+    {
+        int hash = (key % TABLE_SIZE);
+        
+        while(table[hash] != NULL && table[hash]->getKey() != key)
+        {
+            hash = (hash + 1)  % TABLE_SIZE;
+        }
+        if(table[hash] != NULL)
+        {
+            delete table[hash];
+        }
+        
+        table[hash] = new Hash_Entry(key, value);
+    }
+    
+    ~Hash_Map()
+    {
+        for(int i = 0; i < TABLE_SIZE; i++)
+        {
+            if(table[i] != NULL)
+            {
+                delete table[i];
+            }
+            
+            delete table;
+        }
+    }
 };
 
 int main(int argc, char** argv)
