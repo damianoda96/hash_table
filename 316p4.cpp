4d02fa65c04cc9e772479617af723c3dd71919d6
Deven Damiano <damiano.da96@gmail.com>
2017-12-03 10:01:13 -0500

    generation of random sequence of integers implemented

--- 316p4.cpp
+++ 316p4.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Hash_Entry
 {
@@ -22,7 +25,7 @@ struct Hash_Entry
     }
 };
 
-const int TABLE_SIZE = 128;
+const int TABLE_SIZE = 10001;
 
 struct Hash_Map
 {
@@ -86,8 +89,84 @@ struct Hash_Map
     }
 };
 
+struct Sequence_Array
+{
+    int** sequence_array;
+    int size_of_array;
+    
+    Sequence_Array()
+    {
+        sequence_array = NULL;
+    }
+    
+    Sequence_Array(int size)
+    {
+        this->size_of_array = size;
+        sequence_array = new int* [size];
+    }
+    
+    ~Sequence_Array()
+    {
+        for(int i = 0; i < size_of_array; i++)
+        {
+            if (this->sequence_array[i] != NULL)
+            {
+                delete sequence_array[i];
+            }
+        }
+    }
+    
+    void push_back(int* sequence)
+    {
+        for(int i = 0; i < size_of_array; i++)
+        {
+            if(this->sequence_array[i] == NULL)
+            {
+                sequence_array[i] = sequence;
+                break;
+            }
+        }
+    }
+    
+    int size()
+    {
+        return size_of_array;
+    }
+};
+
+int* generatedSequence(int size)
+{
+    int random_integer;
+    
+    int* sequence = new int[size];
+    
+    srand (time(NULL));
+    
+    for(int i = 0; i < size; i++)
+    {
+        random_integer = rand() % 9;
+            
+        sequence[i] = random_integer;
+    }
+    
+    return sequence;
+    
+}
+
 int main(int argc, char** argv)
 {
+    
+    //This is a structure that holds an array of int*
+    Sequence_Array aos(10);
+    
+    //First, generate each sequence
+    for(int i = 1000; i <= 10000; i+=1000)
+    {
+        aos.push_back(generatedSequence(i));
+    }
+    
+    std::cout << "SEQUENCES CREATED\n";
+    
     return 0;
     
 }
